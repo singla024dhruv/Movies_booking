8b6eee0e25716b45f9751349ee07f6db0be94fd6
drj <rajdishu2003@gmail.com>
2025-03-08 12:18:42 +0530

    seat schema added

---
+++ backend/models/seat.js
@@ -0,0 +1,26 @@
+const mongoose=require("mongoose");
+
+const seatSchema=new mongoose({
+    seat_number:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    status:{
+        type:String,
+        enum:["available", "booked"],
+        default:"available"
+    },
+    category:{
+        type:String,
+        enum:["VIP", "Regular"],
+        default:"Regular"
+    },
+    price:{
+        type:Number,
+        required:true
+    }
+});
+
+const Seat= mongoose.model("Seat", seatSchema);
+module.exports=Seat;
